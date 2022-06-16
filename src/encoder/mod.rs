6a6bfd6cf060bd6a0584f887fed29ff0d5367074
Tiaan Louw <tiaanl@gmail.com>
2022-06-16 12:54:08 +0200

    Remove op_code from instructions data.

--- src/encoder/mod.rs
+++ src/encoder/mod.rs
@@ -1,9 +1,8 @@
 mod instructions;
 
 use crate::ast;
+use crate::ast::Operands;
 use crate::lexer::Span;
-use ast::Instruction;
-
 pub use instructions::{str_to_operation, Operation};
 
 #[allow(non_camel_case_types)]
@@ -29,71 +28,75 @@ pub enum Code {
 #[derive(Debug)]
 pub enum EncodeError {
     InvalidOperands(Span),
+
+    Unknown(Span),
 }
 
 impl EncodeError {
     pub fn span(&self) -> &Span {
         match self {
             EncodeError::InvalidOperands(span) => span,
+            EncodeError::Unknown(span) => span,
         }
     }
 }
 
-type Encoder = fn(&Instruction, &InstructionData) -> Result<Vec<u8>, EncodeError>;
-type Sizer = fn(&Instruction, &InstructionData) -> Result<u32, EncodeError>;
+type Encoder = fn(&ast::Instruction, &InstructionData) -> Result<Vec<u8>, EncodeError>;
+type Sizer = fn(&ast::Instruction, &InstructionData) -> Result<u32, EncodeError>;
 
 impl std::fmt::Display for EncodeError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::InvalidOperands(_) => write!(f, "Invalid operands"),
+            Self::Unknown(_) => write!(f, "Unknown"),
         }
     }
 }
 
 #[allow(non_camel_case_types)]
-#[derive(Debug, Eq, PartialEq)]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 pub enum OperandType {
-    none,
+    none = 1,
 
-    one,
+    one = 2,
 
-    imm,
-    imm8,
-    imm16,
+    imm = 3,
+    imm8 = 4,
+    imm16 = 5,
 
-    sbyteword,
-    sbyteword16,
+    sbyteword = 6,
+    sbyteword16 = 7,
 
-    al,
-    ax,
-    cl,
-    cx,
-    dx,
-    reg8,
-    reg16,
+    al = 8,
+    ax = 9,
+    cl = 10,
+    cx = 11,
+    dx = 12,
+    reg8 = 13,
+    reg16 = 14,
 
-    es,
-    cs,
-    ss,
-    ds,
-    seg,
+    es = 15,
+    cs = 16,
+    ss = 17,
+    ds = 18,
+    seg = 19,
 
-    mem,
-    _mem8,
-    mem16,
+    mem = 20,
+    _mem8 = 21,
+    mem16 = 22,
 
-    rm8,
-    rm16,
+    rm8 = 23,
+    rm16 = 24,
 
-    _disp8,
-    _disp16,
+    _disp8 = 25,
+    _disp16 = 26,
 
-    seg_off,
+    seg_off = 27,
 }
 
 pub struct InstructionData {
     pub operation: Operation,
-    pub op_code: u8,
     pub destination: OperandType,
     pub source: OperandType,
     pub encoder: Encoder,
@@ -106,11 +109,12 @@ pub mod funcs {
 
     pub trait EncoderTrait {
         fn encode(
-            instruction: &Instruction,
+            instruction: &ast::Instruction,
             data: &InstructionData,
         ) -> Result<Vec<u8>, EncodeError>;
 
-        fn size(instruction: &Instruction, data: &InstructionData) -> Result<u32, EncodeError>;
+        fn size(instruction: &ast::Instruction, data: &InstructionData)
+            -> Result<u32, EncodeError>;
     }
 
     macro_rules! encoder {
@@ -121,7 +125,7 @@ pub mod funcs {
             impl EncoderTrait for $name {
                 #[inline]
                 fn encode(
-                    instruction: &Instruction,
+                    instruction: &ast::Instruction,
                     data: &InstructionData,
                 ) -> Result<Vec<u8>, EncodeError> {
                     $encode(instruction, data)
@@ -129,7 +133,7 @@ pub mod funcs {
 
                 #[inline]
                 fn size(
-                    instruction: &Instruction,
+                    instruction: &ast::Instruction,
                     data: &InstructionData,
                 ) -> Result<u32, EncodeError> {
                     $size(instruction, data)
@@ -138,21 +142,76 @@ pub mod funcs {
         };
     }
 
-    // fn encode(
-    //     _instruction: &Instruction,
-    //     _data: &InstructionData,
-    // ) -> Result<Vec<u8>, EncodeError> $encode
-    //
-    // fn size(
-    //     _instruction: &Instruction,
-    //     _data: &InstructionData,
-    // ) -> Result<u32, EncodeError> $size
+    pub mod bytes_only {
+        use super::*;
 
-    encoder!(bytes_only, |_, _| { todo!() }, |_, _| { todo!() });
+        pub fn encode(
+            _instruction: &ast::Instruction,
+            data: &InstructionData,
+        ) -> Result<Vec<u8>, EncodeError> {
+            let mut output = vec![];
+
+            for code in data.codes {
+                match code {
+                    Code::byte(byte) => output.push(*byte),
+                    _ => panic!("invalid code"),
+                }
+            }
+
+            Ok(output)
+        }
+
+        pub fn size(
+            _instruction: &ast::Instruction,
+            _data: &InstructionData,
+        ) -> Result<u32, EncodeError> {
+            todo!()
+        }
+    }
+
     encoder!(immediate, |_, _| { todo!() }, |_, _| { todo!() });
     encoder!(memory_and_register, |_, _| { todo!() }, |_, _| { todo!() });
     encoder!(register_and_memory, |_, _| { todo!() }, |_, _| { todo!() });
-    encoder!(memory_immediate, |_, _| { todo!() }, |_, _| { todo!() });
+
+    pub mod memory_immediate {
+        use super::*;
+
+        pub fn encode(
+            instruction: &ast::Instruction,
+            data: &InstructionData,
+        ) -> Result<Vec<u8>, EncodeError> {
+            let mut output = vec![];
+
+            dbg!(data.codes);
+
+            for code in data.codes {
+                match code {
+                    Code::byte(byte) => output.push(*byte),
+                    Code::encoding(encoding) => match &instruction.operands {
+                        Operands::DestinationAndSource(
+                            _,
+                            ast::Operand::Address(_, _, _),
+                            ast::Operand::Immediate(_),
+                        ) => {
+                            todo!()
+                        }
+                        _ => panic!("Invalid operands: {:?}", instruction.operands),
+                    },
+                    _ => panic!("invalid code"),
+                }
+            }
+
+            Ok(output)
+        }
+
+        pub fn size(
+            _instruction: &ast::Instruction,
+            _data: &InstructionData,
+        ) -> Result<u32, EncodeError> {
+            todo!()
+        }
+    }
+
     encoder!(immediate_source, |_, _| { todo!() }, |_, _| { todo!() });
     encoder!(jump_immediate, |_, _| { todo!() }, |_, _| { todo!() });
     encoder!(memory, |_, _| { todo!() }, |_, _| { todo!() });
@@ -161,71 +220,83 @@ pub mod funcs {
     encoder!(register_source, |_, _| { todo!() }, |_, _| { todo!() });
 }
 
-fn operand_to_type(operand: &ast::Operand) -> OperandType {
-    match operand {
-        ast::Operand::Immediate(expr) => {
-            if expr.value() < 256 {
-                OperandType::imm8
-            } else {
-                OperandType::imm16
-            }
+impl<'a> ast::Operand<'a> {
+    fn matches_operand_type(&self, operand_type: OperandType) -> bool {
+        match self {
+            ast::Operand::Immediate(_) => match operand_type {
+                OperandType::one
+                | OperandType::imm
+                | OperandType::imm8
+                | OperandType::imm16
+                | OperandType::sbyteword
+                | OperandType::sbyteword16 => true,
+                _ => false,
+            },
+            ast::Operand::Address(_, _, _) => match operand_type {
+                OperandType::mem
+                | OperandType::_mem8
+                | OperandType::mem16
+                | OperandType::rm8
+                | OperandType::rm16 => true,
+                _ => false,
+            },
+            ast::Operand::Register(_) => match operand_type {
+                OperandType::al
+                | OperandType::ax
+                | OperandType::cl
+                | OperandType::cx
+                | OperandType::dx
+                | OperandType::reg8
+                | OperandType::reg16
+                | OperandType::rm8
+                | OperandType::rm16 => true,
+                _ => false,
+            },
+            ast::Operand::Segment(_) => match operand_type {
+                OperandType::es
+                | OperandType::cs
+                | OperandType::ss
+                | OperandType::ds
+                | OperandType::seg => true,
+                _ => false,
+            },
         }
-        ast::Operand::Address(_, _, _) => todo!(),
-        ast::Operand::Register(register) => match register {
-            ast::Register::Byte(byte_register) => match byte_register {
-                ast::ByteRegister::AL => OperandType::al,
-                _ => OperandType::reg8,
-            },
-            ast::Register::Word(word_register) => match word_register {
-                ast::WordRegister::AX => OperandType::ax,
-                _ => OperandType::reg16,
-            },
-        },
-        ast::Operand::Segment(_) => todo!(),
     }
 }
 
-fn coerce_operand_types(
-    destination: OperandType,
-    source: OperandType,
-) -> (OperandType, OperandType) {
-    use OperandType::*;
-
-    if matches!(destination, imm16 | ax | reg16 | mem16 if source == imm8) {
-        return (destination, imm16);
-    }
-
-    (destination, source)
-}
-
-fn find_instruction_data_for(instruction: &Instruction) -> Option<&'static InstructionData> {
-    let (destination, source) = match &instruction.operands {
-        ast::Operands::None(_) => (OperandType::none, OperandType::none),
-        ast::Operands::Destination(_, destination) => {
-            (operand_to_type(destination), OperandType::none)
-        }
-        ast::Operands::DestinationAndSource(_, destination, source) => {
-            (operand_to_type(destination), operand_to_type(source))
-        }
-    };
-
-    let (destination, source) = coerce_operand_types(destination, source);
-
-    dbg!("searching_for", &destination, &source);
-
+fn find_instruction_data_for(instruction: &ast::Instruction) -> Option<&'static InstructionData> {
     for data in instructions::DATA {
-        if data.operation == instruction.operation
-            && data.destination == destination
-            && data.source == source
-        {
-            return Some(data);
+        if data.operation != instruction.operation {
+            continue;
+        }
+
+        match &instruction.operands {
+            Operands::None(_) => {
+                if data.destination == OperandType::none && data.source == OperandType::none {
+                    return Some(data);
+                }
+            }
+            Operands::Destination(_, destination) => {
+                if data.source == OperandType::none
+                    && destination.matches_operand_type(data.destination)
+                {
+                    return Some(data);
+                }
+            }
+            Operands::DestinationAndSource(_, destination, source) => {
+                if destination.matches_operand_type(data.destination)
+                    && source.matches_operand_type(data.source)
+                {
+                    return Some(data);
+                }
+            }
         }
     }
 
     None
 }
 
-pub fn size_in_bytes<'a>(instruction: &Instruction<'a>) -> Result<u32, EncodeError> {
+pub fn size_in_bytes<'a>(instruction: &ast::Instruction<'a>) -> Result<u32, EncodeError> {
     let data = match find_instruction_data_for(instruction) {
         Some(data) => data,
         None => {
@@ -242,7 +313,7 @@ pub fn size_in_bytes<'a>(instruction: &Instruction<'a>) -> Result<u32, EncodeErr
     })
 }
 
-pub fn encode<'a>(instruction: &Instruction<'a>) -> Result<Vec<u8>, EncodeError> {
+pub fn encode<'a>(instruction: &ast::Instruction<'a>) -> Result<Vec<u8>, EncodeError> {
     let data = match find_instruction_data_for(instruction) {
         Some(data) => data,
         None => {
@@ -254,7 +325,6 @@ pub fn encode<'a>(instruction: &Instruction<'a>) -> Result<Vec<u8>, EncodeError>
     dbg!(
         "found data",
         &data.operation,
-        &data.op_code,
         &data.destination,
         &data.source
     );
